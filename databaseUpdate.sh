8a6279138c3ca12779d331c560db67f68d9d6426
mechalk <mechalk.spam@gmail.com>
2016-08-08 21:47:56 -0400

    Added a tax deductible column to the transactions table along with the options to add it and filter on it

--- databaseUpdate.sh
+++ databaseUpdate.sh
@@ -3,7 +3,23 @@
 if [ -z "$1" ]; then
    echo "Please provide the name of the database file to update"
 else
-   echo "Adding notes column to the income table for the database file $1"
-   sqlite3 $1 'ALTER TABLE income ADD COLUMN notes VARCHAR(1000)'
-   sqlite3 $1 '.schema income'
+   echo "Modifying database file "$1""
+
+   # Add notes column to the income table
+   if [ "$(sqlite3 $1 '.schema income' | grep -c notes)" -gt 0 ]; then
+      echo "Notes already in the income table"
+   else
+      echo "Adding notes column to the income table"
+      sqlite3 $1 'ALTER TABLE income ADD COLUMN notes VARCHAR(1000)'
+      sqlite3 $1 '.schema income'
+   fi
+
+   # Add tax_deduct column to the transactions table
+   if [ "$(sqlite3 $1 '.schema transactions' | grep -c tax_deduct)" -gt 0 ]; then
+      echo "Tax deduct column already in the transactions table"
+   else
+      echo "Adding tax_decuct column to the transaction table"
+      sqlite3 $1 'ALTER TABLE transactions ADD COLUMN tax_deduct INTEGER NOT NULL DEFAULT 0'
+      sqlite3 $1 '.schema transactions'
+   fi
 fi
